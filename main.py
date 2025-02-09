1a92b3c0e2de16f9bb1f861f375c4ad798ce2d16
ChaimShlez <chaimshlez@gmail.com>
2025-02-09 12:00:57 +0200

    first commit

---
+++ main.py
@@ -0,0 +1,7 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+num = int(input(" Please enter a number: "))
+
+for x in range(1,num+1):
+    print(" " +str(x) * x)
